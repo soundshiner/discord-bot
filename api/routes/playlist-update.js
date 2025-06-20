837857e4fbb04a0761090d88777782a0a9d692d1
n3m01726 <kiocubic.rulezz@proton.me>
2025-06-19 21:00:30 -0400

    Mise à jour du texte de pied de page dans la route API de mise à jour des playlists

--- api/routes/playlist-update.js
+++ api/routes/playlist-update.js
@@ -43,7 +43,7 @@ export default (client, logger) => {
         description: `**${playlist}** est maintenant en rotation sur soundSHINE! Vous pouvez l'écouter en direct sur le canal <#1383684854255849613>.`,
         color: 0xaff6e4,
         footer: {
-          text: "soundSHINE Radio | https://soundshineradio.com",
+          text: "https://soundshineradio.com",
           icon_url: "https://soundshineradio.com/avatar.jpg",
         },
       };
