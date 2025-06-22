472c8d8e1286cd7cc2ac74386443a7fbc5c23ffb
n3m01726 <kiocubic.rulezz@proton.me>
2025-06-22 15:28:04 -0400

    fix: log stack trace for invalid Express response object in API error handler

--- utils/errorHandler.js
+++ utils/errorHandler.js
@@ -51,6 +51,7 @@ class ErrorHandler {
   handleApiError(error, req, res) {
     if (typeof res.status !== 'function' || typeof res.json !== 'function') {
       this.logger.error('handleApiError: res is not a valid Express response object');
+      this.logger.error(new Error().stack);
       return;
     }
     const errorId = this.generateErrorId();
